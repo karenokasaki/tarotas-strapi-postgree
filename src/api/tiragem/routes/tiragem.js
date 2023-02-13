0603ee643a5122e50ca991570d6c6d5516119b8b
karen okasaki <oks.karen@gmail.com>
2023-02-13 09:49:12 -0300

    add task

---
+++ src/api/tiragem/routes/tiragem.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * tiragem router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::tiragem.tiragem');
---
+++ src/api/tiragem/services/tiragem.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * tiragem service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::tiragem.tiragem');
